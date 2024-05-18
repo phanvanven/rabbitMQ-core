caa54ec701b5ea0482d19e9bceeff8892368750b
Phan Van Ven <contact.phanvanven@gmail.com>
2024-05-18 20:37:30 +0700

    Update parameter annotations in producer

--- queue/producer.js
+++ queue/producer.js
@@ -11,12 +11,15 @@ const sendQueue = async ({ msg }) => {
         // 4. create queue
         // const queueName = await channel.assertQueue(); The queue name will be created automatically if we don't set it up beforehand
         await channel.assertQueue(queueName,{
-            durable: false // Data in queue will be lost if your server is cracked or shut down suddenly
+            durable: true //If 'durable' is 'false' then data (message) in queue will be lost if your RabbitMQ service is cracked or shut down suddenly.
         });
         // 5. send messages to queue
         // + Buffer sends data as bytes so it will send data much faster. 
         // + Data encryption is supported by buffer, which can enhance data security even more.
-        await channel.sendToQueue(queueName, Buffer.from(msg)); 
+        await channel.sendToQueue(queueName, Buffer.from(msg), {
+            // expiration: '10000', // TTL,
+            persistent: true // The message will be handled constantly, and the queue will be stored on disk or in a cache. If there is a problem with caching, then RabbitMQ will use disk to run.
+        }); 
         // 6. close connection and channel
 
         
